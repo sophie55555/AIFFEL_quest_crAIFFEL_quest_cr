6f7a7138c8c3d8c3f715b74eccb82a69d439e2cd
sophie5 <113117898+sophie55555@users.noreply.github.com>
2025-01-24 15:51:41 +0900

    Add files via upload

---
+++ Flutter/quest03/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: CatScreen()
+    );
+  }
+}
+
+class CatScreen extends StatelessWidget{
+  bool is_cat = true;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Page"),
+        centerTitle: true, // 타이틀을 중앙으로 정렬
+        backgroundColor: Colors.blue, // AppBar 색상
+        leading: Icon(FontAwesomeIcons.cat), // 왼쪽 상단 아이콘
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DogScreen())
+                ); // 클릭 시 DEBUG 콘솔 출력
+              },
+              child: Text(
+                "Next",
+                style: TextStyle(fontSize: 40),
+              ), // 버튼 텍스트
+            ),
+            SizedBox(height: 20), // 버튼과 Stack 사이 간격
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: GestureDetector(
+                  onTap: (){
+                    print(is_cat);
+                  },
+                  child: Image.asset(
+                    'assets/images/cat.jpeg',
+                    width: 300,
+                    height: 300,
+
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DogScreen extends StatelessWidget{
+  bool is_cat = false;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+        centerTitle: true, // 타이틀을 중앙으로 정렬
+        backgroundColor: Colors.blue, // AppBar 색상
+        leading: Icon(FontAwesomeIcons.dog), // 왼쪽 상단 아이콘
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Back",
+                style: TextStyle(fontSize: 40),
+              ), // 버튼 텍스트
+            ),
+            SizedBox(height: 20), // 버튼과 Stack 사이 간격
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: GestureDetector(
+                  onTap: (){
+                    print(is_cat);
+                  },
+                  child: Image.asset(
+                    'assets/images/dog.jpeg',
+                    width: 300,
+                    height: 300,
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//회고
+//고명지:Navigator.push와 Navigator.pop을 사용해 화면 전환 구현을 해보아 의미있었음.
+// 다만, is_cat 변수에 대한 이해는 좀 더 시간이 필요함.
+//김민상: ClipRRect를 사용하여 사진의 라운드값을 넣는법을 배웠다.
